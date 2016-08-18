4881c29c7752bc8dc68a86f0671b76794831c15c
Zeno Rocha <hi@zenorocha.com>
2016-08-18 15:19:45 -0700

    Adds API Client logics

---
+++ hosting/main.js
@@ -0,0 +1,22 @@
+var form = document.querySelector('form');
+
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+
+	WeDeploy
+		.url('http://email.emaildemo.wedeploy.io/emails')
+		.form('from', form.from.value)
+		.form('to', form.to.value)
+		.form('subject', form.subject.value)
+		.post()
+		.then(function(response) {
+			form.reset();
+
+			alert('Email sent! Wait a little bit until it arrives :)')
+
+			console.info('Email ID:', response.body());
+		})
+		.catch(function(error) {
+			console.error(error);
+		});
+});
